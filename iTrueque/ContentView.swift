2941b584299937bab8c5ba6a757b20e45626bb21
Osvaldo Chaparro Bolaños <osvaldo.chaparro@tymit.com>
2021-09-26 21:19:05 +0100

    Initial Commit

---
+++ iTrueque/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iTrueque
+//
+//  Created by Osvaldo Chaparro Bolaños on 26/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
