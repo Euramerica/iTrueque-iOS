f13b28c0104d68ec978e19cd7edb079ce9a59464
Osvaldo Chaparro Bolaños <osvaldo.chaparro@tymit.com>
2021-09-28 23:54:10 +0100

    IntroView - Vista básica para la pantalla de Intro - Sin funcionalidades todavía, solo integrada en el FlowCoordinator

---
+++ iTrueque/Presentation/IntroScene/View/IntroView.swift
@@ -0,0 +1,83 @@
+//
+//  IntroView.swift
+//  iTrueque
+//
+//  Created by Osvaldo Chaparro Bolaños on 27/9/21.
+//
+
+import SwiftUI
+
+struct IntroView: View {
+    
+    var body: some View {
+        VStack{
+            VStack{
+                Image("itrueque-logo")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(35)
+            }.padding(.top, 80)
+            
+            Spacer()
+            
+            Button(action: {
+                
+            }) {
+                HStack(spacing: 70){
+                    Image("apple-logo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                    Text("Continue_with_apple".localized()).foregroundColor(.black)
+                    Spacer()
+                }
+                .frame(width: .infinity, height:20, alignment: .center)
+                .padding(10)
+            }
+            .frame(maxWidth: .infinity)
+            .background(Color.white)
+            .cornerRadius(14)
+            .padding(.leading, 15)
+            .padding(.trailing, 15)
+            .padding(.bottom, 5)
+            
+            Button(action: {
+                
+            }) {
+                HStack(spacing: 70){
+                    Image(systemName: "mail").foregroundColor(.black)
+                    Text("Continue_with_email".localized()).foregroundColor(.black)
+                    Spacer()
+                }
+                .frame(width: .infinity, height:20, alignment: .center)
+                .padding(10)
+            }
+            .background(Color.mainColor)
+            .cornerRadius(14)
+            .padding(.leading, 15)
+            .padding(.trailing, 15)
+            .padding(.bottom, 50)
+            
+            HStack{
+                Text("Already_has_account".localized()).foregroundColor(.white)
+                Button(action: {
+                    
+                }) {
+                    Text("Login".localized()).foregroundColor(.mainColor).bold()
+                }
+            }
+            .padding(.bottom, 50)
+            
+        }
+        .frame(maxWidth: .infinity)
+        .background(Image("background-intro"))
+        .edgesIgnoringSafeArea(.all)
+        .padding(.bottom, 30)
+    }
+}
+
+// MARK:- Preview
+struct IntroView_Previews: PreviewProvider {
+    static var previews: some View {
+        IntroView()
+    }
+}
