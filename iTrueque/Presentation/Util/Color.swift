9306d83f65c7df01cce7c05de41ea0456591051b
rodri2d2 <rodrigoipu@hotmail.com>
2021-10-16 20:44:22 +0200

    Add components color.

--- iTrueque/Presentation/Util/Color.swift
+++ iTrueque/Presentation/Util/Color.swift
@@ -8,7 +8,9 @@
 import SwiftUI
 
 public extension Color {
-    static let mainColor       = Color(#colorLiteral(red: 0.08235294118, green: 0.7490196078, blue: 0.6274509804, alpha: 1))
-    static let backgroundColor = Color(#colorLiteral(red: 0.2235294118, green: 0.3450980392, blue: 0.3921568627, alpha: 1))
-    static let bodyColor       = Color(#colorLiteral(red: 0.5411764706, green: 0.6196078431, blue: 0.6509803922, alpha: 1))
+    static let mainColor            = Color(#colorLiteral(red: 0.08235294118, green: 0.7490196078, blue: 0.6274509804, alpha: 1))
+    static let backgroundColor      = Color(#colorLiteral(red: 0.2235294118, green: 0.3450980392, blue: 0.3921568627, alpha: 1))
+    static let bodyColor            = Color(#colorLiteral(red: 0.5411764706, green: 0.6196078431, blue: 0.6509803922, alpha: 1))
+    static let secondaryBackground  = Color(#colorLiteral(red: 0.9490196078, green: 0.9490196078, blue: 0.9490196078, alpha: 1))
+    static let textTitleColor       = Color(#colorLiteral(red: 0.1725490196, green: 0.2274509804, blue: 0.2509803922, alpha: 1))
 }
