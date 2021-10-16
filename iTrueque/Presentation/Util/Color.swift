7d72a510c1c6075147218fe6f98c7efd4cc49bfe
Osvaldo Chaparro Bolaños <osvaldo.chaparro@tymit.com>
2021-10-17 00:49:41 +0100

    Se ha creado un componente nuevo Toast que a su vez contiene un componente Card. Además, se ha aplicado este nuevo componente a la vista login para los errores devueltos por Firebase

--- iTrueque/Presentation/Util/Color.swift
+++ iTrueque/Presentation/Util/Color.swift
@@ -11,4 +11,5 @@ public extension Color {
     static let mainColor       = Color(#colorLiteral(red: 0.08235294118, green: 0.7490196078, blue: 0.6274509804, alpha: 1))
     static let backgroundColor = Color(#colorLiteral(red: 0.2235294118, green: 0.3450980392, blue: 0.3921568627, alpha: 1))
     static let bodyColor       = Color(#colorLiteral(red: 0.5411764706, green: 0.6196078431, blue: 0.6509803922, alpha: 1))
+    static let light           = Color(#colorLiteral(red: 0.937254902, green: 0.937254902, blue: 0.937254902, alpha: 1))
 }
