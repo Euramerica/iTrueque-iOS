8fe0a8639c57b0e6e654af5a651647471215cdc0
rodri2d2 <rodrigoipu@hotmail.com>
2021-10-18 22:15:42 +0200

    Create a gender repo.

---
+++ iTrueque/Domain/Interfaces/Repositories/GenderRepository.swift
@@ -0,0 +1,43 @@
+//
+//  GenderRepository.swift
+//  iTrueque
+//
+//  Created by Rodrigo Candido on 18/10/21.
+//
+
+import Foundation
+import Combine
+import Firebase
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+
+final class GenderRepository{
+    
+    let path = "genre"
+    private let store = Firestore.firestore()
+    
+    func getGenres() -> [Genre]{
+        
+        var genres: [Genre] = []
+        
+        store.collection(path)
+            .addSnapshotListener { query, error in
+                if let error = error{
+                  print("Error getting genres: \(error.localizedDescription)")
+                  return
+                }
+                
+                let genresDoc = query?.documents.compactMap{ document in
+                    try? document.data(as: Genre.self)
+                }
+                
+                genres = genresDoc ?? []
+                
+            }
+        
+        return genres
+
+    }
+    
+}
