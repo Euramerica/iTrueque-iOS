f0402511f983be0ee498c6176b541af37efb264f
Osvaldo Chaparro Bolaños <osvaldo.chaparro@tymit.com>
2021-10-21 01:55:33 +0100

    Vista Profile con el caso de uso de obtener datos de usuario y viewModel

--- iTrueque/Presentation/ProfileScene/View/ProfileView.swift
+++ iTrueque/Presentation/ProfileScene/View/ProfileView.swift
@@ -12,11 +12,33 @@ struct ProfileView: View {
     var viewModel: AnyViewModel<ProfileState, ProfileAction>
     
     var body: some View {
-        Button(action: {
-            viewModel.handle(.logout)
-        }) {
-            Text("logout")
+        HStack{
+            if let avatar = URL(string: viewModel.state.user.avatar) {
+                URLImage(
+                    url: avatar,
+                    placeholder: Rectangle().background(Color.light),
+                    configuration: { $0.resizable() }
+                )
+                .scaledToFit()
+                .padding(.all, 4)
+                .frame(width: 100, height: 100)
+            }
+            VStack(alignment: .leading, spacing: 5) {
+                Text(viewModel.state.user.name).font(.title)
+                Text(viewModel.state.user.surname).font(.title2)
+                Text(viewModel.state.user.email).font(.body)
+                Button(action: {
+                    viewModel.handle(.logout)
+                }) {
+                    Text("logout")
+                }
+            }
+            
+            
+        }.onAppear() {
+            viewModel.handle(.onAppear)
         }
+        
 
     }
     
--- iTrueque/Presentation/ProfileScene/ViewModel/ProfileViewModel.swift
+++ iTrueque/Presentation/ProfileScene/ViewModel/ProfileViewModel.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct ProfileState {
     var dataState: ModelDataState = .idle
+    var user: UserApp = UserApp(id: "", uid: "", name: "", surname: "", email: "", password: "", avatar: "")
     
     mutating func changeViewModelState(newViewModelState: ModelDataState) {
         dataState = newViewModelState
@@ -19,6 +20,8 @@ struct ProfileState {
 enum ProfileAction {
     case logout
     case goToIntro
+    case getProfileData
+    case onAppear
 }
 
 class ProfileViewModel: ViewModel {
@@ -26,6 +29,7 @@ class ProfileViewModel: ViewModel {
     private var cancellableSet: Set<AnyCancellable> = []
     private let removeStoredLogin = RemoveStoredLogin()
     private let coordinatorActions: TabBarViewModelActions?
+    private let getProfileData = GetProfileData()
     
     @Published
     var state: ProfileState
@@ -39,6 +43,9 @@ class ProfileViewModel: ViewModel {
     
     func handle(_ action: ProfileAction) {
         switch action {
+        case .onAppear:
+            self.handle(.getProfileData)
+            
         case .logout:
             removeStoredLogin.execute()
                 .sink { [weak self] completion in
@@ -56,6 +63,20 @@ class ProfileViewModel: ViewModel {
             
         case .goToIntro:
             coordinatorActions?.showIntro()
+            
+        case .getProfileData:
+            getProfileData.execute()
+                .sink { completion in
+                    switch completion {
+                    case .finished:
+                        print("getProfileData finished")
+                    case .failure(let error):
+                        print(error)
+                    }
+                } receiveValue: { [weak self] user in
+                    self?.state.user = user
+                }
+                .store(in: &cancellableSet)
         }
     }
     
